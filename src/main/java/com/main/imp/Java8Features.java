1b222649484d0c5b3541857d96504d8b694bd39e
Dheeraj <loggerspace@gmail.com>
2025-07-10 20:21:25 +0530

    Push

--- src/main/java/com/main/imp/Java8Features.java
+++ src/main/java/com/main/imp/Java8Features.java
@@ -1,5 +1,36 @@
 package com.main.imp;
 
+import java.util.List;
+import java.util.function.Consumer;
+
+import com.main.classes.PersonDetails;
+
 public class Java8Features {
 
+	public static void main(String[] args) {
+		Java8Features j = new Java8Features();
+		j.defaultFuntionalInterfaces();
+
+	}
+
+	public void defaultFuntionalInterfaces() {
+		Runnable r = () -> System.out.println("Thread running with lambda!");
+		new Thread(r).start();
+
+		// we can directly give in thread argument as below
+		new Thread(() -> System.out.println("Thread running!")).start();
+
+		// Consumer accept(); forEach() has consumer argument
+		Consumer<String> c = (s) -> System.out.println(s.toUpperCase());
+		c.accept("Hello Dheeraj");
+
+		PersonDetails dheeraj = new PersonDetails("Dheeraj", "33", "08-December");
+		PersonDetails ravi = new PersonDetails("Ravi", "34", "10-January");
+		List<PersonDetails> personsList = List.of(dheeraj, ravi);
+		Consumer<PersonDetails> c1 = (s) -> System.out.print(s.name() + " ");
+		Consumer<PersonDetails> c2 = (s) -> System.out.print(s.age() + " ");
+		Consumer<PersonDetails> c3 = (s) -> System.out.print(s.Dob() + " ");
+		// Consumer chaining
+		personsList.forEach(c1.andThen(c2).andThen(c3));
+	}
 }
