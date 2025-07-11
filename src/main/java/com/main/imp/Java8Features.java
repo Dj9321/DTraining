6420f81aa148f97e368e0b24bc1a969c24af72e0
Dheeraj <loggerspace@gmail.com>
2025-07-11 16:18:52 +0530

    Pushing

--- src/main/java/com/main/imp/Java8Features.java
+++ src/main/java/com/main/imp/Java8Features.java
@@ -2,7 +2,9 @@ package com.main.imp;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import com.main.classes.PersonDetails;
 
@@ -11,8 +13,17 @@ public class Java8Features {
 	public static void main(String[] args) {
 		Java8Features j = new Java8Features();
 		j.defaultFuntionalInterfaces();
+		j.defaultFuncIntPredicate();
 
 	}
+
+	PersonDetails dheeraj = new PersonDetails("Dheeraj", "33", "08-December");
+	PersonDetails ravi = new PersonDetails("Ravi", "34", "10-January");
+	List<PersonDetails> personsList = List.of(dheeraj, ravi);
+	Consumer<PersonDetails> c1 = (s) -> System.out.println(s.name() + " ");
+	Consumer<PersonDetails> c2 = (s) -> System.out.print(s.age() + " ");
+	Consumer<PersonDetails> c3 = (s) -> System.out.println(s.Dob() + " ");
+	Consumer<PersonDetails> c4 = c1.andThen(c2).andThen(c3);
 
 	public void defaultFuntionalInterfaces() {
 		Runnable r = () -> System.out.println("Thread running with lambda!");
@@ -24,18 +35,9 @@ public class Java8Features {
 		// Consumer accept(); forEach() has consumer argument
 		Consumer<String> c = (s) -> System.out.println(s.toUpperCase());
 		c.accept("Hello Dheeraj");
-
-		PersonDetails dheeraj = new PersonDetails("Dheeraj", "33", "08-December");
-		PersonDetails ravi = new PersonDetails("Ravi", "34", "10-January");
-		List<PersonDetails> personsList = List.of(dheeraj, ravi);
-		Consumer<PersonDetails> c1 = (s) -> System.out.println(s.name() + " ");
-		Consumer<PersonDetails> c2 = (s) -> System.out.print(s.age() + " ");
-		Consumer<PersonDetails> c3 = (s) -> System.out.println(s.Dob() + " ");
-
 		// You can call a consumer on any object so that the logic will be executed on
 		// that object
 		c1.accept(dheeraj);
-		Consumer<PersonDetails> c4 = c1.andThen(c2).andThen(c3);
 		// Consumer chaining
 		personsList.forEach(c1.andThen(c2).andThen(c3));
 
@@ -58,6 +60,41 @@ public class Java8Features {
 		// Here we can't pass biconsumer directly to forEach so we pass as Consumer
 		// lambda
 		personsList.forEach(f -> b.accept(f, f));
+	}
 
+	public void defaultFuncIntPredicate() {
+		// Predicate
+		System.out.println("Predicate: ");
+		Predicate<Integer> p1 = p -> p % 2 == 0;
+		Predicate<Integer> p2 = p -> p % 5 == 0;
+
+		boolean b1 = p1.test(15);
+		System.out.println(b1);
+
+		boolean b2 = p1.and(p2).test(50);
+//		or() in Predicate: 
+		boolean b3 = p1.or(p2).test(33);
+		System.out.println(b2 + " " + b3);
+
+//		negate() in Predicate:
+		boolean b4 = p1.or(p2).negate().test(33);
+		System.out.println("b4: " + b4);
+
+		Predicate<PersonDetails> pp = g -> Integer.valueOf(g.age()) > 33;
+		personsList.forEach(bp -> {
+//			we can use predicate here instead of the if-condition
+//			if (Integer.valueOf(b.age()) > 33) {
+			if (pp.test(bp)) {
+				c4.accept(bp);
+			}
+		});
+
+		BiPredicate<PersonDetails, Integer> bip = (g1, g2) -> Integer.valueOf(g1.age()) > g2;
+		System.out.println("Bi Predicate: ");
+		personsList.forEach(bp -> {
+			if (bip.test(bp, 21)) {
+				c4.accept(bp);
+			}
+		});
 	}
 }
