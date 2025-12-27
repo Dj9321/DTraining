ec8ed323b35a83d190f84878489cbb20f5e498d4
Dheeraj <loggerspace@gmail.com>
2025-12-27 21:47:32 +0530

    DF

--- src/main/java/com/main/imp/DateAndTime.java
+++ src/main/java/com/main/imp/DateAndTime.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.Period;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -40,29 +41,117 @@ public class DateAndTime {
 		LocalDateTime dateTime4 = LocalDateTime.of(date1, time1);
 
 //		LocalDate d = new LocalDate(); // DOES NOT COMPILE
-		LocalDate.of(2015, Month.JANUARY, 32); // throws DateTimeException Invalid value for DayOfMonth (valid values 1
-												// - 28/31): 32
+//		LocalDate.of(2015, Month.JANUARY, 32); // throws DateTimeException Invalid value for DayOfMonth (valid values 1
+		// - 28/31): 32
 // 		OLD way:  The Date class represented both the date and time whether you wanted it to or not.
 		Date d = new Date();
-		
+
 		// creating a particular date in OLD way
 		Calendar c = Calendar.getInstance();
-		c.set(2015, Calendar.­JANUARY, 1);
+		c.set(2015, 0, 1); // OLD ways starts with 0
 		Date jan = c.getTime();
-		
-		// Manipulating Dates and Times
+
+		// Manipulating Dates and Times > plus Days, Weeks, Months, Years
 		LocalDate date = LocalDate.of(2014, Month.JANUARY, 20);
-		System.out.println(date);          // 2014-01-20
+		System.out.println(date); // 2014-01-20
 		date = date.plusDays(2);
-		System.out.println(date);          // 2014-01-22
+		System.out.println(date); // 2014-01-22
 		date = date.plusWeeks(1);
-		System.out.println(date);          // 2014-01-29
-		// Java is smart enough to realize February 29, 2014 does not exist and gives us February 28, 2014 instead. 
+		System.out.println(date); // 2014-01-29
+		// Java is smart enough to realize February 29, 2014 does not exist and gives us
+		// February 28, 2014 instead.
 		date = date.plusMonths(1);
-		System.out.println(date);          // 2014-02-28
+		System.out.println(date); // 2014-02-28
 		date = date.plusYears(5);
-		System.out.println(date);          // 2019-02-28
+		System.out.println(date); // 2019-02-28
 
+		// minus Days, Hours, Minutes, Seconds, Nanos
+		LocalDate date3 = LocalDate.of(2020, Month.JANUARY, 20);
+		LocalTime time = LocalTime.of(5, 15);
+		LocalDateTime dateTime = LocalDateTime.of(date3, time);
+		System.out.println(dateTime); // 2020-01-20T05:15
+		dateTime = dateTime.minusDays(1);
+		System.out.println(dateTime); // 2020-01-19T05:15
+		dateTime = dateTime.minusHours(10);
+		System.out.println(dateTime); // 2020-01-18T19:15
+		dateTime = dateTime.minusSeconds(30);
+		// Displays seconds as we are using seconds in above line.
+		System.out.println(dateTime); // 2020-01-18T19:14:30
 
+		// Chaining methods:
+		LocalDateTime dateTime5 = LocalDateTime.of(date2, time).minusDays(1).minusHours(10).minusSeconds(30);
+
+		// Whenever you see immutable types, pay attention to make sure the return value
+		// of a method call isn't ignored.
+		LocalDate date4 = LocalDate.of(2020, Month.JANUARY, 20);
+		date4.plusDays(10);
+		System.out.println(date4);
+
+		// Date does not contain time
+//		LocalDate date = LocalDate.of(2020, Month.JANUARY, 20);
+//		date = date.plusMinutes(1);     // DOES NOT COMPILE
+
+		LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
+		LocalDate end = LocalDate.of(2015, Month.MARCH, 30);
+		// isBefore()
+		performAnimalEnrichment(start, end);
+
+		// LocalDate > toEpochDay(), LocalDateTime has toEpochTime() > Time from Jan 1
+		// 1970 GMT
+		// LocalTime doesn't have this method as it represents a time that occurs on any
+		// date
+		System.out.println(date2.toEpochDay());
+//		System.out.println(dateTime.toEpochSecond());
+
+		// Period:
+		Period period = Period.ofMonths(1); // create a period
+		performAnimalEnrichment(start, end, period);
+
+//		There are five ways to create a Period class:
+		Period annually = Period.ofYears(1); // every 1 year
+		Period quarterly = Period.ofMonths(3); // every 3 months
+		Period everyThreeWeeks = Period.ofWeeks(3); // every 3 weeks
+		Period everyOtherDay = Period.ofDays(2); // every 2 days
+		Period everyYearAndAWeek = Period.of(1, 0, 7); // every year and 7 days
+		// There's one catch. You cannot chain methods when creating a Period.
+
+//		Period wrong = Period.ofYears(1).ofWeeks(1);          // every week  DOESN'T work
+//		Period wrong = Period.ofYears(1);
+//		wrong = Period.ofWeeks(7);
+
+		LocalDate date5 = LocalDate.of(2015, 1, 20);
+		LocalTime time4 = LocalTime.of(6, 15);
+		LocalDateTime dateTime6 = LocalDateTime.of(date5, time4);
+		Period period1 = Period.ofMonths(1);
+		System.out.println(date5.plus(period1)); // 2015-02-20
+		System.out.println(dateTime6.plus(period1)); // 2015-02-20T06:15
+//		System.out.println(time4.plus(period)); // UnsupportedTemporalTypeException > Unsupported unit: Months
+		
+		// More information from Date: getDayOfWeek(), getMonth(), getYear(), getDayOfYear()
+		LocalDate date6 = LocalDate.of(2020, Month.JANUARY, 20);
+		System.out.println(date6.getDayOfWeek());     // MONDAY
+		System.out.println(date6.getMonth());          // JANUARY
+		System.out.println(date6.getYear());          // 2020
+		System.out.println(date6.getDayOfYear());     // 20
+		
+//		To format date the way we like we can use DateTimeFormatter.
+		
+	}
+
+	private static void performAnimalEnrichment(LocalDate start, LocalDate end, Period period) { // uses the generic
+																									// period
+		LocalDate upTo = start;
+		while (upTo.isBefore(end)) {
+			System.out.println("give new toy: " + upTo);
+			upTo = upTo.plus(period); // adds the period
+		}
+	}
+
+	private static void performAnimalEnrichment(LocalDate start, LocalDate end) {
+		LocalDate upTo = start;
+		while (upTo.isBefore(end)) { // check if still before end
+			System.out.println("give new toy: " + upTo);
+			upTo = upTo.plusMonths(1); // add a month
+		}
 	}
 }
